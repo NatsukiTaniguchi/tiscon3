1f612719ec36c1fb1afdd2021194a1a1102ec535
cookie1234monster <b817120701@kpu.ac.jp>
2018-02-18 15:21:37 +0900

    Not Brank消す

--- src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
+++ src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
@@ -59,38 +59,38 @@ public class CardOrderForm extends FormBase {
     @Size(max = 20)
     private String spouse;
 
-    @NotBlank
+    
     @Size(max = 20)
     private String houseClassification;
 
-    @NotBlank
+
     @Size(max = 6)
     @Pattern(regexp = "[0-9]*")
     private String debt;
 
-    @NotBlank
+
     @Size(max = 120)
     private String job;
 
-    @NotBlank
+
     @Size(max = 6)
     @Pattern(regexp = "[0-9]*")
     private String income;
 
-    @NotBlank
+
     @Size(max = 255)
     private String employerName;
 
-    @NotBlank
+
     @Size(max = 8)
     @Pattern(regexp = "^([0-9]{3}-[0-9]{4})?$")
     private String employerZipCode;
 
-    @NotBlank
+
     @Size(max = 255)
     private String employerAddress;
 
-    @NotBlank
+
     @Size(max = 13)
     @Pattern(regexp = "^(0[0-9]{1,3}-[0-9]{2,4}-[0-9]{4})?$")
     private String employerPhoneNumber;
@@ -99,25 +99,25 @@ public class CardOrderForm extends FormBase {
     @Size(max = 255)
     private String industryType;
 
-    @NotBlank
+
     @Size(max = 6)
     @Pattern(regexp = "[0-9]*")
     private String capital;
 
-    @NotBlank
+
     @Size(max = 255)
     @Pattern(regexp = "[0-9]*")
     private String companySize;
 
-    @NotBlank
+
     @Size(max = 255)
     private String position;
 
-    @NotBlank
+
     @Size(max = 255)
     private String department;
 
-    @NotBlank
+
     @Size(max = 6)
     @Pattern(regexp = "[0-9]*")
     private String employeeLength;
